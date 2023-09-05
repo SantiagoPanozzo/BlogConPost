4f73924d8ffb88d698517fe242a4203d561fe8e9
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-09-05 10:01:05 -0300

    hecho, falta estilo y organizar

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 import { pool } from '../dbconnection.js'
 export default async function (fastify, opts) {
-    fastify.get('/blog', async function (request, reply) {
-        const query = await pool.query('SELECT * FROM Posts;');
-        const articulos = await query.rows;
-        return reply.send(articulos);
-        //return reply.view("templates/blog.ejs", { articulos });
-    })
+    fastify.get('/posts', async function (request, reply) {
+        return reply.view("templates/formulario.ejs");
+    });
+    fastify.post('/posts', async function (request, reply) {
+        const { titulo, descripcion, imagen, alternativo, cuerpo, autor } = request.body;
+        const query = await pool.query('INSERT INTO posts (titulo, descripcion, imagen, alternativo, cuerpo, autor) VALUES ($1, $2, $3, $4, $5, $6);', [titulo, descripcion, imagen, alternativo, cuerpo, autor]);
+        return reply.send({ message: 'Post creado' });
+    });
 }
--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,10 @@
+import { pool } from '../dbconnection.js'
+
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
+    fastify.get('/', async function (request, reply) {
+        const query = await pool.query('SELECT * FROM posts;');
+        const articulos = await query.rows;
+        //return reply.send(articulos);
+        return reply.view("templates/blog.ejs", { articulos });
+    });
 }
